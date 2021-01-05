8e420b18e051a7d92169c426305a343aca99b1fc
Adhiti <shaili.adhiti@gmail.com>
2021-01-05 16:20:42 +0530

    Basic

---
+++ Basic/Anagram palindrome.cpp
@@ -0,0 +1,37 @@
+#include<unordered_map>
+using namespace std;
+
+int main() {
+	//code
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    string s;
+	    cin>>s;
+	    int count=0;
+	    unordered_map<char,int> hash;
+	    for(int i=0;i<s.size();i++)
+	    {
+	        hash[s[i]]++;
+	    }
+	    
+	    for(auto i=hash.begin();i!=hash.end();i++)
+	    {
+	        if((i->second%2)!=0)
+	        {
+	            count++;
+	        }
+	    }
+	    if(count>1)
+	    {
+	        cout<<"No";
+	    }
+	    else
+	    {
+	        cout<<"Yes";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
