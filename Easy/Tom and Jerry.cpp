ad5cd677fd1a6dfc59c78df2397aa08e539a56a0
Adhiti <shaili.adhiti@gmail.com>
2021-01-11 14:47:59 +0530

    Wins first

---
+++ Easy/Tom and Jerry.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    if(n%2==0)
+	    {
+	        cout<<1;
+	    }
+	    else
+	    {
+	        cout<<0;
+	    }
+	    cout<<endl;
+	  
+	}
+	return 0;
+}
